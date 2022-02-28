3689ac88c986be8da9f7675a2f127d8f87187c7d
SurTan02 <suryanto.tan5@gmail.com>
2022-03-01 01:26:36 +0700

    fix std_lib

--- src/c/std_lib.c
+++ src/c/std_lib.c
@@ -7,12 +7,14 @@
 // Tips : Gunakan C integer division
 int div(int a, int b)
 {
-    return a / b;
+    return a/b;
 }
 
 int mod(int a, int n)
 {
-    return a % n;
+	while (a >= n)
+		a -= n;
+	return a;
 }
 
 
@@ -36,9 +38,7 @@ unsigned int strlen(char *string)
 // Mengembalikan panjang suatu null terminated string
 {
     int n = 0;
-    char *c = string;
-    
-    while (c != '\0')
+    while (string[n] != '\0')
         n++;
     return n;
 }
@@ -47,29 +47,23 @@ unsigned int strlen(char *string)
 bool strcmp(char *s1, char *s2)
 // Mengembalikan true jika string sama
 {
-    int flag, n;
-    char *c1 = s1;
-    char *c2 = s2;
-    
-    flag = strlen(s1) == strlen(s2);
-    n = strlen(s1);
-    while (flag && n--)
+    int i = 0;
+    int flag = true;    
+    while (flag)
     {
-        flag = (*c1 == *c2);
-        *c1++;
-        *c2++;
+        if (s1[i] != s2[i]) flag = false;
+        if (s1[i] == '\0')  break;
+        i++;
     }
     return flag;
+
 }
 
 
 void strcpy(char *dst, char *src)
 // Melakukan penyalinan null terminated string
 {
-    char *temp = dst;
-    // while(*dst++ = *src++); // or 
     while((*dst++=*src++) != '\0');
-    // return *temp;
 }
 void clear(byte *ptr, unsigned int n)
 // Mengosongkan byte array yang memiliki panjang n
